4dfd82c68c68bf7fa1274d17c7e375b60094cce1
D4v3-ita <davin874@gmail.com>
2021-02-15 21:35:17 +0100

    ho modificato il terzo file

--- filippo/Es1c/src/Main.java
+++ filippo/Es1c/src/Main.java
@@ -15,10 +15,10 @@ public class Main {
         System.out.println("Crea il tuo PIN");
         pin = tastiera.nextLine();
 
+        // metodo per creare array (char) del pin appena creato
         char[] arrpincreato = crearePin(pin, tastiera);
 
 
-        // ---------------------------------------------------------------------------------------------------------
 
         // conterrà i valori da 0 a 9 random
         int[] numrand = new int[10];
@@ -35,40 +35,34 @@ public class Main {
         System.out.println();
         System.out.println();
 
-        // ---------------------------------------------------------------------------------------------------------
-
-
 
         // reinserimento PIN per "accedere"
         System.out.println("Inserisci il tuo PIN per accedere");
         String pinxaccedere = tastiera.nextLine();
 
+
+        // metodo per creare array (char) del pin reinserito
         char[] arrpinxaccedere = reinserirePin(pinxaccedere, tastiera);
 
 
-
-        // ---------------------------------------------------------------------------------------------------------
-
-
-
+        // metodo per controllare se il pin inserito è corretto o no
         int sbagliato = confrontaPin(arrpinxaccedere, numrand, arrpincreato);
+
 
         // se anche solo una cifra è stata digitata male, stampa messaggio di errore ...
         // ... altrimenti, se tutto è giusto, stampa il PIN originale
         if(sbagliato!=0){
             System.out.println("PIN NON VALIDO");
-            System.out.println(sbagliato);
         } else {
             for(i=0;i< arrpincreato.length;i++){
                 System.out.print(arrpincreato[i] + " ");
             }
-            System.out.println(sbagliato);
         }
 
     }
 
 
-// -----------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------
     public static char[] crearePin(String pin, Scanner tastiera){
         int i;
 
@@ -90,7 +84,7 @@ public class Main {
 
     }
 
-// ----------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------
     public static char[] reinserirePin(String pinxaccedere, Scanner tastiera){
         int i;
 
@@ -113,12 +107,12 @@ public class Main {
     }
 
     // --------------------------------------------------------------------------------
-
     public static int confrontaPin(char[] arrpinxaccedere, int[] numrand, char[] arrpincreato){
         int i;
         int l;
-// contatore inverso per le cifre sbagliate
+        // contatore inverso per le cifre sbagliate
         int sbagliato = 5;
+
         for(i=0;i<arrpinxaccedere.length;i++){
             for(i=0;i<arrpincreato.length;i++){
                 for(l=0;l<10;l++){
@@ -126,15 +120,11 @@ public class Main {
                         // il contatore diminuisce se la cifra inserita è giusta
                         sbagliato--;
                     }
-
                 }
-
             }
-
         }
 
         return sbagliato;
-
     }
 
 
