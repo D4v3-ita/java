b5ac629d265e50c40e82012d6d7a27ed73f699d5
Davide Andreana <davin874@gmail.com>
2021-02-26 12:33:55 +0100

    aggiungo strutture-dati

--- filippo/Strutture-dati/src/Nil.java
+++ filippo/Strutture-dati/src/Nil.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Nil {
+// SERVE PER CONCLUDERE LA LISTA, POICHÉ LA LISTA È DINAMICA
+public class Nil extends List{
+    @Override
+    public String toString() {
+        // stampiamo nulla poiché la lista sarà finita
+        return "";
+
+        // metodo ricorsivo, va all'infinito (in realtà si ferma prima di occupare ...
+        // ... tutta quanta la RAM
+//        return "" + this.toString();
+    }
+
+    @Override
+    public List addLast(int x) {
+        return new Node(x, this);
+    }
+
+    @Override
+    public void addLast2(int x) {
+    }
 }
